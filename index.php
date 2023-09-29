c863cda344d17c6f3dfb290c41e252e198c095c2
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-29 15:54:27 +0200

    feat: movie1/2/3

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ class Movie {
     public $genere;
     public $annoUscita;
 
-    // public function 
+    public function __construct() {
+        
+    }
 }
 
 $movie_1 = new Movie();
@@ -13,4 +15,14 @@ $movie_1->titolo = 'Storia di un Matrimonio';
 $movie_1->genere = 'Drammatico';
 $movie_1->annoUscita = 2019;
 
-var_dump($movie_1);
+$movie_2 = new Movie();
+$movie_2->titolo = 'Parasite';
+$movie_2->genere = 'Commedia';
+$movie_2->annoUscita = 2018;
+
+$movie_3 = new Movie();
+$movie_3->titolo = 'Blade Runner 2049';
+$movie_3->genere = 'Thriller';
+$movie_3->annoUscita = 2017;
+
+var_dump($movie_1, $movie_2, $movie_3);
