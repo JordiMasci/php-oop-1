c1acaf605df4f5415d440f4007b414117c369070
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-29 16:01:12 +0200

    feat: construct

--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <?php
 
-class Movie {
+class Movie
+{
     public $titolo;
     public $genere;
     public $annoUscita;
 
-    public function __construct() {
-        
+    public function __construct($titolo, $genere, $annoUscita)
+    {
+        $this->titolo = $titolo;
+        $this->genere = $genere;
+        $this->annoUscita = $annoUscita;
     }
 }
 
-$movie_1 = new Movie();
-$movie_1->titolo = 'Storia di un Matrimonio';
-$movie_1->genere = 'Drammatico';
-$movie_1->annoUscita = 2019;
+$movie_1 = new Movie('Storia di un Matrimonio', 'Drammatico', 2019);
 
-$movie_2 = new Movie();
-$movie_2->titolo = 'Parasite';
-$movie_2->genere = 'Commedia';
-$movie_2->annoUscita = 2018;
 
-$movie_3 = new Movie();
-$movie_3->titolo = 'Blade Runner 2049';
-$movie_3->genere = 'Thriller';
-$movie_3->annoUscita = 2017;
+$movie_2 = new Movie('Parasite', 'Commedia', 2018);
+
+
+$movie_3 = new Movie('Blade Runner 2049', 'Thriller', 2017);
+
 
 var_dump($movie_1, $movie_2, $movie_3);
