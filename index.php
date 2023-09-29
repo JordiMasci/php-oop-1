acd06f0d3a255440978795df590d867d4c5d1911
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-29 16:18:09 +0200

    feat: metodo per stampare dettagli films

--- index.php
+++ index.php
@@ -3,18 +3,23 @@
 // Apro classe php
 class Movie
 {
-    // Variabili di istanza
+    // Variabili di istanza (attributi)
     public $titolo;
     public $genere;
     public $annoUscita;
 
-    // Apro un costrutto
+    // Apro un costrutto (metodo)
     // serve ad istanziare gli oggetti
     public function __construct(string $titolo, string $genere, int $annoUscita)
     {
         $this->titolo = $titolo;
         $this->genere = $genere;
         $this->annoUscita = $annoUscita;
+    }
+
+    // Function per stampare Info Movies (metodo)
+    public function InfoMovies() {
+        return "Titolo: $this->titolo, Genere: $this->genere, Anno Di Uscita: $this->annoUscita";
     }
 }
 
@@ -24,4 +29,4 @@ $movie_2 = new Movie('Parasite', 'Commedia', 2018);
 $movie_3 = new Movie('Blade Runner 2049', 'Thriller', 2017);
 
 
-var_dump($movie_1, $movie_2, $movie_3);
+echo ($movie_1->InfoMovies());
