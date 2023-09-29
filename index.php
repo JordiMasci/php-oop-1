bae60b113fb244f2c9057d3c9da01da4b01bbf5c
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-29 16:08:19 +0200

    feat: commenti

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
+// Apro classe php
 class Movie
 {
+    // Variabili di istanza
     public $titolo;
     public $genere;
     public $annoUscita;
 
+    // Apro un costrutto
+    // serve ad istanziare gli oggetti
     public function __construct(string $titolo, string $genere, int $annoUscita)
     {
         $this->titolo = $titolo;
@@ -14,12 +18,9 @@ class Movie
     }
 }
 
+// Istanza di 3 oggetti 
 $movie_1 = new Movie('Storia di un Matrimonio', 'Drammatico', 2019);
-
-
 $movie_2 = new Movie('Parasite', 'Commedia', 2018);
-
-
 $movie_3 = new Movie('Blade Runner 2049', 'Thriller', 2017);
 
 
