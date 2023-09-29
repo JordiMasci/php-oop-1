e859503b7b50df48d596aae6449cccf0ff2c5e32
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-29 16:21:06 +0200

    feat: stampa a schermo movies

--- index.php
+++ index.php
@@ -18,7 +18,8 @@ class Movie
     }
 
     // Function per stampare Info Movies (metodo)
-    public function InfoMovies() {
+    public function InfoMovies()
+    {
         return "Titolo: $this->titolo, Genere: $this->genere, Anno Di Uscita: $this->annoUscita";
     }
 }
@@ -29,4 +30,32 @@ $movie_2 = new Movie('Parasite', 'Commedia', 2018);
 $movie_3 = new Movie('Blade Runner 2049', 'Thriller', 2017);
 
 
-echo ($movie_1->InfoMovies());
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Lista Movies</h1>
+        <ul>
+            <li><?php echo ($movie_1->InfoMovies()); ?></li>
+            <li><?php echo ($movie_2->InfoMovies()); ?></li>
+            <li><?php echo ($movie_3->InfoMovies()); ?></li>
+        </ul>
+    </div>
+</body>
+
+</html>
